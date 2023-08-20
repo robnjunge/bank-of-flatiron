bfeaa089e7738e14edfe8b8d93381bd0a9e9983a
robertnjunge <mubearobert@gmail.com>
2023-08-21 00:39:31 +0300

    added comments

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,38 +1,28 @@
 import React from 'react';
 
-
 function TransactionList({ transactions }) {
   return (
     <div className="transaction-list">
       <h2>Transaction List</h2>
       <table>
         <thead>
-         <tr>
-          <td>
-            Date
-          </td>
-          <td>
-            Description
-          </td>
-          <td>
-            Category
-          </td>
-          <td>
-            Amount
-          </td>
-        </tr>   
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
         </thead>
         <tbody>
-        {transactions.map(transaction => (
-          <tr key={transaction.id} className="transaction-item">
-            <td>{transaction.date}</td>
-            <td>{transaction.description}</td>
-            <td>{transaction.category}</td>
-            <td>{transaction.amount}</td>
-          </tr>
-        ))}
-          
-       </tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id} className="transaction-item">
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
