212c97bcbd79cd5b24a5ff035944c4eeb642f9a9
robertnjunge <mubearobert@gmail.com>
2023-08-20 15:51:45 +0300

    added component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import Home from './views/Home';
 import React from 'react';
 import './App.css'
-import Transactions from './components/Transactions';
-import TransactionList from './components/TransactionList';
+
+
 function App() {
-  
 
   return (
     <div className="App">
-     <Transactions /> 
+      <Home />
     </div>
   );
 }
---
+++ src/views/Home.js
@@ -0,0 +1,9 @@
+import  React, {useState, useEffect} from 'react'
+import NewTransactions from '..components/NewTransactions'
+import TransactionList from '..components/TransactionList'
+
+function Home(){
+ 
+}
+
+export default Home
