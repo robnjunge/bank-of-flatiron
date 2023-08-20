3ff4bdef9303da085a9d09511931ca5def69ec81
robertnjunge <mubearobert@gmail.com>
2023-08-21 01:10:09 +0300

    added delete button

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,36 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import NewTransactions from '../components/NewTransactions';
-import TransactionList from '../components/TransactionList'
+import TransactionList from '../components/TransactionList';
 
 function Home() {
   const [allTransactions, setAllTransactions] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch(`http://localhost:8001/transactions?q=${searchQuery}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
-        setAllTransactions(data)})
+        console.log(data);
+        setAllTransactions(data);
+      })
       .catch(error => console.error(error));
   }, [searchQuery]);
 
-  function handleSearch (e) {
-    setSearchQuery(e.target.value)
+  function handleSearch(e) {
+    setSearchQuery(e.target.value);
   }
 
-  function handleDeleteTransaction(transactionId) {
-    const updatedTransactions = allTransactions.filter(transaction => transaction.id !== transactionId);
-    setAllTransactions(updatedTransactions);
+  async function handleDeleteTransaction(transactionId) {
+    try {
+      await fetch(`http://localhost:8001/transactions/${transactionId}`, {
+        method: 'DELETE',
+      });
+
+      setAllTransactions(prevTransactions =>
+        prevTransactions.filter(transaction => transaction.id !== transactionId)
+      );
+    } catch (error) {
+      console.error('Error deleting transaction:', error);
+    }
   }
 
   return (
     <div>
       <div>
-        <input id="input" type="text" placeholder="Search Here..." onChange={handleSearch} />
+        <input
+          id="input"
+          type="text"
+          placeholder="Search Here..."
+          onChange={handleSearch}
+        />
         <button id="searchButton">Search</button>
       </div>
-      <TransactionList transactions={allTransactions} onDeleteTransaction={handleDeleteTransaction} />
+      <TransactionList
+        transactions={allTransactions}
+        onDeleteTransaction={handleDeleteTransaction}
+      />
       <NewTransactions />
     </div>
   );
